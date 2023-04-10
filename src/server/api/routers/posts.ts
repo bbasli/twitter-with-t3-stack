4f260b5f32fba44333f99c3f8f758cae1ff2c713
bbasli <bgrbasli@gmail.com>
2023-04-11 01:21:35 +0300

    Update getAll route to fetch user data for posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,28 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import type { User } from "@clerk/nextjs/dist/api";
+
+const filterUserForClient = (user: User) => {
+  const { id, username, profileImageUrl } = user;
+
+  return { id, username, profileImageUrl };
+};
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
 });
