d1d735d43887f9f74e9374756d4ac25b4650e75f
bbasli <43937051+bbasli@users.noreply.github.com>
2023-04-13 23:58:27 +0300

    Add getStaticProps

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,17 +1,42 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 const ProfilePage: NextPage = () => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    id: "user_2OFduXeGQOD8xr5BEt2USzDXf3Z",
+  });
+
   return (
     <>
       <Head>
         <title>Profile</title>
       </Head>
       <main className="flex	min-h-screen justify-center">
-        <div>Profile View</div>
+        <div>{data?.username}</div>
       </main>
     </>
   );
+};
+import { createSSGHelpers } from "@trpc/react/ssg";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+
+export const getStaticProps = async (context) => {
+  const ssg = createSSGHelpers({
+    router: appRouter,
+    ctx: { prisma },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") {
+    throw new Error("Slug must be a string");
+  }
+
+  await ssg.profile.getUserByUsername.prefetch({ id: slug });
 };
 
 export default ProfilePage;
