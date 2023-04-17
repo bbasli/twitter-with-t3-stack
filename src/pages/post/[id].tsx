2b50dd1063f437ed50218a7ceace112d935a8e57
bbasli <bgrbasli@gmail.com>
2023-04-17 23:22:33 +0300

    Update individual post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,21 +1,47 @@
+import Head from "next/head";
 import type { NextPage } from "next";
 
-import Head from "next/head";
+import { api } from "~/utils/api";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import { PostView } from "~/components/post-view";
 
-dayjs.extend(relativeTime);
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getPostById.useQuery({ id });
 
-const SinglePostPage: NextPage = () => {
+  if (!data) return <h1>404!!!</h1>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content ?? "-"} - ${data.author.username}`}</title>
       </Head>
-      <main className="flex	min-h-screen justify-center">SinglePostPage</main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+import type { GetServerSidePropsContext } from "next";
+import { PageLayout } from "~/components/layout";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const helpers = generateSSGHelper();
+  const id = context.params?.id as string;
+
+  if (!id) throw new Error("No id");
+
+  await helpers.posts.getPostById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      id,
+    },
+  };
 };
 
 export default SinglePostPage;
